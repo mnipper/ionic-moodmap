4db61dc02071e2137d239c3cf0c435cbce12d988
mn127 <mjn4406@gmail.com>
2015-02-09 15:11:53 -0500

    Add templates for home and mood

--- www/js/app.js
+++ www/js/app.js
@@ -9,3 +9,19 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives',
     $uuid.getUUID();
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('index', {
+    url: '/',
+    templateUrl: 'templates/home.html',
+    controller: 'MapCtrl'
+  })
+
+  .state('mood', {
+    url: '/mood',
+    templateUrl: 'templates/mood.html'
+  });
+
+  $urlRouterProvider.otherwise('/');
+});
