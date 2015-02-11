f385219349f0e89f0c57ae424038ffe7c6d65dc0
Leonard Ng'eno <dranoel.bravo@gmail.com>
2015-02-11 17:46:36 -0500

    draw mood circles on map

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,25 @@
 angular.module('starter.controllers', [])
 
-.controller('MapCtrl', function($scope, $ionicLoading, locationService) {
+.controller('MapCtrl', ['$scope', '$ionicLoading', 'locationService', 'MoodItem', 'Mood',
+                        function($scope, $ionicLoading, locationService, MoodItem, Mood) {
 	$scope.myLocation;
+	$scope.moods = {};
 	$scope.mapCreated = function(map) {
 		$scope.map = map;
 		$scope.centerOnMe();
 	};
+	
+	Mood.getAll().success(function(data) {
+		for (var i = 0; i < data.results.length; i++) {
+			var item = data.results[i];
+			$scope.moods[item.objectId] = {'label': item.label, 'color': item.color };
+		}
+	});
+	
+	MoodItem.getAll().success(function(data) {
+		$scope.moodItems = data.results;
+		$scope.drawMoods();
+	});
 
 	$scope.centerOnMe = function () {		
 		if (!$scope.map) {
@@ -33,7 +47,27 @@ angular.module('starter.controllers', [])
 			alert('Unable to get location: ' + error.message);
 		});
 	};
-})
+	
+	$scope.drawMoods = function() {
+		for (var i = 0; i < $scope.moodItems.length; i++) {
+			var item = $scope.moodItems[i];  
+			var mood = $scope.moods[item.mood];
+			new google.maps.Marker({
+				  position: new google.maps.LatLng(item.location.latitude, item.location.longitude),
+				  map: $scope.map,
+				  title: mood.label,
+				  icon: {
+				      path: google.maps.SymbolPath.CIRCLE,
+				      fillColor: mood.color,
+				      fillOpacity: 1.0,
+				      strokeColor: mood.color,
+				      scale: 7
+				    }
+			  });
+		}
+	};
+	
+}])
 
 .controller('MoodSliderCtrl', ['$scope', '$uuid', '$ionicSlideBoxDelegate', 'MoodItem', 'Mood', 'locationService', 
                                function($scope, $uuid, $ionicSlideBoxDelegate, MoodItem, Mood, locationService) {  
