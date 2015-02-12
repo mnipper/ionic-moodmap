a48e1f98056077cdce671e40e3e3d3e0af8d8711
Leonard Ng'eno <dranoel.bravo@gmail.com>
2015-02-12 15:11:36 -0500

    app name

--- www/js/directives.js
+++ www/js/directives.js
@@ -9,8 +9,8 @@ angular.module('starter.directives', [])
     link: function ($scope, $element, $attr) {
       function initialize() {
         var mapOptions = {
-          center: new google.maps.LatLng(43.07493, -89.381388),
-          zoom: 16,
+          center: new google.maps.LatLng(36.000483, -78.938496),
+          zoom: 15,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map($element[0], mapOptions);
